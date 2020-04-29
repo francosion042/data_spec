fed4e3af68297dded26ce279d1999a1527304aa7
Anthony Nwobodo <anthonynwobodo7@gmail.com>
2020-04-30 00:21:51 +0100

    ft-set up for heroku deployment

--- data_spec/settings.py
+++ data_spec/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+q#a0jpa5ybup$o6$sklo5#02d&36qx03f%f@nt+!to#jydsyi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'data-spec-api.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
